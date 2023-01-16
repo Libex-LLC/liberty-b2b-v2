992e8b5594f3267ce0c6c7892c51dd39bb1820d7
cvijandj <cvijandjdj@gmail.com>
2023-01-16 14:19:22 +0100

    completely removed TW from components

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -7,25 +7,33 @@ import styled from 'styled-components';
 
 function MenuCard({place, onClick, color}) {
     const Div = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-radius: 2rem;
-  border-width: 4px;
-  border-color: gray;
- background-color: ${color};
-  width: 20rem;
-  height: 15rem;
-  justify-content: center;
- 
-`;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 2rem;
+    border-width: 4px;
+    border-color: gray;
+    background-color: ${color};
+    width: 20rem;
+    height: 15rem;
+    justify-content: center;
+    
+    `;
+      const P = styled.p`
+     margin-top: 3rem;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  font-weight: 500;
+  font-family: ui-serif, Georgia, 'Times New Roman', Times, serif;
+  color: rgba(255 255 255);
+      `;
     
     return (
         <Div  onClick={onClick} 
         >
           <LoginIcon sx={{ marginTop:4, color: 'white', alignSelf: 'center', fontSize: 90 }}/>
             
-            <p className=' mt-12 text-white text-2xl  '> {place}</p>
+            <P> {place}</P>
         </Div>
     );
 }
--- src/screens/LandingUser.jsx
+++ src/screens/LandingUser.jsx
@@ -8,35 +8,44 @@ function LandingUser(props) {
   flex-direction: column;
   align-items: center;
   background-color: #3f3f46 ;
- 
+  
   width: 100vw;
   height: 100vh;
   
-`;  
-const Div1 = styled.div`
+  `;  
+  const Div1 = styled.div`
   display: flex;
   justify-content: space-around;
   opacity: 0.9;
   margin-top:2.5rem ;
- 
+  
   width: 100vw;
   
   
-`;  
-const Div2 = styled.div`
+  `;  
+  const Div2 = styled.div`
   display: flex;
   justify-content: space-around;
   opacity: 0.9;
   margin-top:1.5rem ;
- 
   width: 80vw;
   
   
-`; 
- 
+  `; 
+  
+  const H1 = styled.h1`
+  margin-top: 4.5rem;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  font-weight: 500;
+  font-family: ui-serif, Georgia, 'Times New Roman', Times, serif;
+  color: rgba(255 255 255);
+  
+  `; 
+
     return (
         <Div>
-          <h1 className='text-white font-serif font-medium text-2xl mt-24'>Make a selection below.</h1>
+          <H1>Make a selection below.</H1>
           <Div1>
           <MenuCard onClick={() => console.log('visitor check in')} color={'#3f7092'}  place={"Visitor Check In"}/>
           <MenuCard onClick={() => console.log('visitor family ')} color={'#568f65 '}  place={"Visitor Family Check In"}/>
